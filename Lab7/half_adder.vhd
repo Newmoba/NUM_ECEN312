5247ce400b8f86096fd9524ea7cdfd868e1c0086
Newmoba <90805142+Newmoba@users.noreply.github.com>
2025-10-24 13:15:50 +0800

    Implement half adder in VHDL

---
+++ Lab7/half_adder.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity half_adder is
+  port(x, y: in std_logic;
+       s, c: out std_logic);
+end half_adder:
+
+architecture dataflow_3 of half_adder is
+  begin
+    s<= x xor y;
+    c<= x and y;
+end dataflow_3;
