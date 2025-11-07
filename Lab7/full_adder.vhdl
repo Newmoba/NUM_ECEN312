3f08316a3475203d0842a1967f8be0fa7aa08c5c
Newmoba <90805142+Newmoba@users.noreply.github.com>
2025-11-07 14:41:38 +0800

    Create full_adder.vhdl

---
+++ Lab7/full_adder.vhdl
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity full_adder is
+  port( x,y,z : in std_logic;
+       s,c : out std_logic);
+end full_adder;
+
+architecture struc_dataflow_3 of full_adder is
+  component half_adder
+    port(x,y : std_logic
+        s,c  : std_logic);
+  end component;
+  signal hs, hc, tc : std_logic;
+  begin
+    HA1: half_adder port map (x,yhs,hc);
+    HA2: half_adder port map (hs,z,s,tc);
+    c <= tc or hs;
+end struc_dataflow_3;
