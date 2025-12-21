9bc6af0b4f2ce67d5567fb8653175a581b3c6ea9
Newmoba <90805142+Newmoba@users.noreply.github.com>
2025-12-21 13:16:34 +0800

    Update rg_8_load_tb.vhdl

--- Lab4/rg_8_load_tb.vhdl
+++ Lab4/rg_8_load_tb.vhdl
@@ -11,13 +11,12 @@ architecture testbench of rg_8_load_tb is
             D : in std_logic_vector(7 downto 0);
             Q : out std_logic_vector(7 downto 0));
     end component;
-
+    
     signal clk_tb : std_logic := '0';
     signal clear_tb : std_logic := '1';
     signal load_tb : std_logic := '0';
     signal D_tb : std_logic_vector(7 downto 0) := (others => '0');
     signal Q_tb : std_logic_vector(7 downto 0);
-    
     
 begin
     uut: rg_8_load 
@@ -28,58 +27,50 @@ begin
             D => D_tb,
             Q => Q_tb
         );
+    process(clear_tb, clk_tb)
+    begin
+	--reset
+        if clear_tb = '0' then
+            D_tb <= "11111111";
+            load_tb <= '1';
+         -- rising edge shalgah
+        elsif (clk_tb'event and clk_tb='1') then
+            if Q_tb = "00000000" then
+                -- after reset, release clear
+                clear_tb <= '1';
+                D_tb <= "10101010";
+                load_tb <= '1';
+                
+            elsif Q_tb = "10101010" then
+                -- new data
+                D_tb <= "11001100";
+                load_tb <= '1';
+                
+            elsif Q_tb = "11001100" then
+                -- disable load(orolt orj irehgui)
+                load_tb <= '0';
+                D_tb <= "00110011";
+                
+            elsif Q_tb = "11001100" and load_tb = '0' then
+                -- shineer ehluuleh
+                load_tb <= '1';
+                D_tb <= "01010101";
+                
+            elsif Q_tb = "01010101" then
+                D_tb <= "11110000";
+                load_tb <= '1';
+                
+            elsif Q_tb = "11110000" then
+                clear_tb <= '0';
+            end if;
+        end if;
+    end process;
     
-    
-    clk_process: process
+    process
     begin
         clk_tb <= '0';
         wait for 20 ns;
         clk_tb <= '1';
         wait for 20 ns;
     end process;
-    
-    -- Stimulus process
-    stim_process: process
-    begin
-        -- Test 1: Reset (clear='0')
-        clear_tb <= '0';
-        D_tb <= "11111111";
-        load_tb <= '1';
-        wait for 20 ns;
-        
-        -- Test 2: Release clear
-        clear_tb <= '1';
-        wait for 20 ns;
-        
-        -- Test 3: Load data when load='1'
-        load_tb <= '1';
-        D_tb <= "10101010";
-        wait for 20 ns;
-        
-        -- Test 4: Try to load new data (load='1')
-        D_tb <= "11001100";
-        wait for 20 ns;
-        
-        -- Test 5: Disable load (load='0'), data should not change
-        load_tb <= '0';
-        D_tb <= "00110011";
-        wait for 20 ns;
-        
-        -- Test 6: Enable load again
-        load_tb <= '1';
-        D_tb <= "01010101";
-        wait for 20 ns;
-        
-        -- Test 7: Clear during operation
-        clear_tb <= '0';
-        wait for 20 ns;
-        
-        -- Test 8: Resume after clear
-        clear_tb <= '1';
-        load_tb <= '1';
-        D_tb <= "11110000";
-        wait for 20 ns;
-        wait;
-    end process;
-    
 end testbench;
